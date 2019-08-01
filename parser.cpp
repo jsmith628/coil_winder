388cf3a4d1566bb4d6ab882cbeda475ead3bb3fa
Joshua Smith <anvil775@gmail.com>
2019-08-01 01:13:11 -0500

    fixed bug when converting floats to flags

--- parser.cpp
+++ parser.cpp
@@ -47,7 +47,7 @@ void interpret_gcode(command c){
         g21();
         break;
       case 28:
-        g28((c.modifiers[A].f != 0), (c.modifiers[B].f != 0));
+        g28((c.modifiers[A].f == c.modifiers[A].f), (c.modifiers[B].f == c.modifiers[B].f));
         break;
       case 31:
         g31((int8_t)c.modifiers[A].f,(int8_t)(c.modifiers[B].f!=0));
