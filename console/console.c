2ea186d0dea9bf67ac3a17a7f4e35fbb24f14cf7
Joshua Smith <anvil775@gmail.com>
2019-08-14 04:21:30 -0500

    added more documentation and a help-screen

--- console/console.c
+++ console/console.c
@@ -18,15 +18,19 @@
 
 #include "../firmware/ascii_control.h"
 
+//controls if output is on or off given the flow control system
 pthread_mutex_t flow_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t flow_ready = PTHREAD_COND_INITIALIZER;
 bool flow_on = true;
 
+//notifies the threads that there is a new command to send
 pthread_mutex_t cmd_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cmd_ready = PTHREAD_COND_INITIALIZER;
 bool cmd_on = false;
 
-pthread_mutex_t buf_lock = PTHREAD_MUTEX_INITIALIZER;
+//
+//Helper methods for thread-stuff
+//
 
 void lock_set(pthread_mutex_t* lock, bool* obj, bool val) {
   pthread_mutex_lock(lock);
@@ -47,6 +51,11 @@ void notify(pthread_cond_t* cond, pthread_mutex_t* lock, bool* value) {
   pthread_mutex_unlock(lock);
 }
 
+
+//the mutex synchronizing buffer access
+pthread_mutex_t buf_lock = PTHREAD_MUTEX_INITIALIZER;
+
+//the buffer of commands
 struct buf_list {
   char buf[BUF_PAGE_SIZE];
   size_t size;
@@ -56,6 +65,7 @@ struct buf_list {
 struct buf_list* cmd_buffer_back;
 struct buf_list* cmd_buffer_front;
 
+//mallocs up a new buffer for the next command line
 struct buf_list* init_page() {
   struct buf_list* page = malloc(sizeof(struct buf_list));
   page->next = NULL;
@@ -64,16 +74,18 @@ struct buf_list* init_page() {
   return page;
 }
 
-bool xflow = true;
-bool echo = false;
+bool xflow = true; //setting for if XON/XOFF works
+bool echo = false; //setting for if things are spat back at stdout
 
-pthread_mutex_t eof_lock = PTHREAD_MUTEX_INITIALIZER;
-bool end_on_eof = false;
+//the setting for if an EOT from the device closes the application
+//however, this needs to be able to change since ^D overrides this behavior
+pthread_mutex_t eot_lock = PTHREAD_MUTEX_INITIALIZER;
+bool end_on_eot = false;
 
-
+//the fd of the device
 int device;
 
-
+//specifies if the application should stop
 pthread_mutex_t stop_lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t stop_ready = PTHREAD_COND_INITIALIZER;
 bool stop = false;
@@ -86,10 +98,9 @@ void *cmd_thread(void* arg) {
     //read the next character
     char x;
     size_t count = fread(&x, 1, 1, stdin);
-    if(count==0 || x==EOT){
-      x = EOT;
-      lock_set(&eof_lock, &end_on_eof, true);
-    }
+
+    //if stdin was closed, set x to EOT
+    if(count==0 || x==EOT) x = EOT;
 
     pthread_mutex_lock(&buf_lock);
 
@@ -159,9 +170,14 @@ void *write_thread(void* arg) {
       lock_set(&cmd_lock, &cmd_on, false);
       wait_until(&cmd_ready, &cmd_lock, &cmd_on);
     } else {
+
+      //if we're sending an EOT, we need to be able to exit when we get a response
+      if(cmd_buffer_back->control) lock_set(&eot_lock, &end_on_eot, true);
+
       //write the line to the device
       write(dev, cmd_buffer_back->buf, cmd_buffer_back->size);
       if(echo && !cmd_buffer_back->control) fwrite(cmd_buffer_back->buf, 1, cmd_buffer_back->size, stdout);
+
 
       //pop the line off of the buffer
       struct buf_list* next = cmd_buffer_back->next;
@@ -170,7 +186,6 @@ void *write_thread(void* arg) {
 
       pthread_mutex_unlock(&buf_lock);
 
-      // usleep(1000);
     }
 
 
@@ -190,49 +205,62 @@ void *read_thread(void* arg) {
     size_t count = read(dev, &x, 1);
 
     if(count) { //make sure no error happened
-      pthread_mutex_lock(&eof_lock);
+      pthread_mutex_lock(&eot_lock);
       if(xflow && x==XON) { //unblock the writing thread if we get an XON
         notify(&flow_ready, &flow_lock, &flow_on);
       } else if(xflow && x==XOFF) { //block the writing thread if we get an XOFF
         lock_set(&flow_lock, &flow_on, false);
-      } else if((end_on_eof && x==EOT) || x==ETX) {
+      } else if((end_on_eot && x==EOT) || x==ETX) { //stop on an EOF, ^D, M20, or ^C
         notify(&stop_ready, &stop_lock, &stop);
       } else { //else, just parrot to stdout
         fputc(x, stdout);
       }
-      pthread_mutex_unlock(&eof_lock);
+      pthread_mutex_unlock(&eot_lock);
     }
 
   }
 }
 
-int init_device(int device) {
-  //configure the serial port
+//configure the serial port
+int init_device(int device, bool conv_lc) {
 
+  //get the current config
   struct termios config;
-
   if(tcgetattr(device, &config)) {
     printf("Error getting serial port config!\n");
     return 1;
   }
 
+  //don't convert anything and don't do XON/XOFF in the driver (since it doesn't work :( )
   config.c_iflag &= ~(IGNBRK | ICRNL | IMAXBEL | INLCR | IUTF8 | IXANY | IUCLC | IXOFF | IXON);
   config.c_iflag |= BRKINT;
 
-  config.c_oflag &= ~(OCRNL | ONLCR | NLDLY);
-  config.c_oflag |= NL0 | OPOST | OLCUC;
+  //don't convert any characters
+  config.c_oflag &= ~(OCRNL | ONLCR | NLDLY | OLCUC);
+  config.c_oflag |= NL0 | OPOST;
 
+  //convert lower-case console input if desired
+  if(conv_lc) config.c_oflag |= OLCUC;
+
+  //115200 baud, 1 stop-bit, no-parity
   config.c_cflag &= ~(CSTOPB | PARENB | CSIZE | CBAUD);
   config.c_cflag |= CRTSCTS | CS8 | B115200;
 
+  //don't echo anything since the output _from_ the device is considered the "input"
+  //so echoing would cause all of the loging information to be fed right back to
+  //the device as a command
+  //also, no flushing, flushing bad
   config.c_lflag &= ~(ECHO | ECHOCTL | ECHOE | ECHOK | ECHONL | ICANON | FLUSHO | NOFLSH | TOSTOP | ISIG | IEXTEN);
   config.c_lflag |= NOFLSH;
 
-  // config.c_cc[VEOL] = '\n';
+  //this isn't really used since it doesn't seem to work :/
   config.c_cc[VSTART] = XON;
   config.c_cc[VSTOP] = XOFF;
+
+  //no read timeout
   config.c_cc[VTIME] = 0;
 
+  //turn on poll-based reads for the startup handshake
   config.c_cc[VMIN] = 0;
   if(tcsetattr(device, TCSANOW, &config)) {
     printf("Error configuring serial port!\n");
@@ -242,7 +270,8 @@ int init_device(int device) {
   //delay until we get a message or time-out to make sure we've properly connected
   for(int i=0, x=0; i<5000 && !read(device, &x, 1); i++) usleep(1000);
 
-  config.c_cc[VTIME] = 0;
+  //turn on blocking-based reads
+  config.c_cc[VMIN] = 1;
   if(tcsetattr(device, TCSANOW, &config)) {
     printf("Error configuring serial port!\n");
     return 1;
@@ -250,6 +279,39 @@ int init_device(int device) {
 
 }
 
+int print_help_info(FILE * stream) {
+  fprintf(stream,
+    "\n"
+    "Usage: console DEVICE [-e|--echo|-E|--noecho] [-x|--xflow|-X|--noxflow] [-d|--eot|-D|--noeot] [-u|--upper|-U|--noupper]\n"
+    "   or: console --help\n"
+    "\n"
+    "Options:\n"
+    "    DEVICE:\n"
+    "        the serial port of the machine to open\n"
+    "    -e, --echo\n"
+    "        when on, all lines written to device are also written to stdout (off by default)\n"
+    "    -E, --noecho:\n"
+    "        turns off --echo\n"
+    "    -x, --xflow:\n"
+    "        turns on XON/XOFF flow control (on by default)\n"
+    "    -X, --noxflow:\n"
+    "        turns off --xflow\n"
+    "    -d, --eot:\n"
+    "        when on, the console will close when an ASCII value 0x04 (EOT) is read from the device \n"
+    "    -D, --noeot:\n"
+    "        turns off --eot\n"
+    "    -u, --upper:\n"
+    "        when on, lower-case input will automatically be converted to uppercase \n"
+    "    -U, --noupper:\n"
+    "        turns off --upper\n"
+    "    --help:\n"
+    "        displays this message\n"
+    "\n"
+  );
+  return 0;
+}
+
+//captures the SIGINT signal and passes it onto the device
 void signal_handler(int sig) {
 
   char msg[2] = "\0\n";
@@ -265,23 +327,30 @@ int main(int argc, char const *argv[]) {
 
   //parse the command line options
   char const * device_name = NULL;
+  bool conv_lc = false;
   for(int i=1; i<argc; i++) {
     if(!strcmp(argv[i],"--echo")) echo=true;
     else if(!strcmp(argv[i],"--noecho")) echo=false;
     else if(!strcmp(argv[i],"--xflow")) xflow=true;
     else if(!strcmp(argv[i],"--noxflow")) xflow=false;
-    else if(!strcmp(argv[i],"--eof")) end_on_eof=true;
-    else if(!strcmp(argv[i],"--eof")) end_on_eof=false;
+    else if(!strcmp(argv[i],"--eot")) end_on_eot=true;
+    else if(!strcmp(argv[i],"--noeot")) end_on_eot=false;
+    else if(!strcmp(argv[i],"--upper")) conv_lc=true;
+    else if(!strcmp(argv[i],"--noupper")) conv_lc=false;
+    else if(!strcmp(argv[i],"--help")) return print_help_info(stdout);
     else if(argv[i][0] == '-' && argv[i][1] != '-' && argv[i][1] != '\0') {
       for(char const * x = &argv[i][1]; *x!='\0'; x++) {
         if(*x=='e') echo=true;
         else if(*x=='E') echo=false;
         else if(*x=='x') xflow=true;
         else if(*x=='X') xflow=false;
-        else if(*x=='d') end_on_eof=true;
-        else if(*x=='D') end_on_eof=false;
+        else if(*x=='d') end_on_eot=true;
+        else if(*x=='D') end_on_eot=false;
+        else if(*x=='u') conv_lc=true;
+        else if(*x=='U') conv_lc=false;
         else {
-          fprintf(stderr, "Invalid option \'%s\'\n", argv[i]);
+          fprintf(stderr, "Invalid option %s\n", argv[i]);
+          fprintf(stderr, "Use --help for more information\n");
           return 1;
         }
       }
@@ -289,6 +358,7 @@ int main(int argc, char const *argv[]) {
       device_name = argv[i];
     } else {
       fprintf(stderr, "Invalid option \"%s\"\n", argv[i]);
+      fprintf(stderr, "Use --help for more information\n");
       return 1;
     }
   }
@@ -306,14 +376,15 @@ int main(int argc, char const *argv[]) {
     return 1;
   }
 
-  int err = init_device(device);
+  //initialize the termios settings a wait for the device to startup
+  int err = init_device(device, conv_lc);
   if(err) return err;
 
-
+  //register a function to handle system signals
   signal(SIGINT, signal_handler);
 
+  //started
   pthread_t t1,t2;
-
   if(pthread_create(&t1, NULL, read_thread, &device) || pthread_create(&t2, NULL, write_thread, &device)){
     fprintf(stderr, "Thread creation error!\n");
     return 1;
